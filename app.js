020fd2bde272fdb023ffb1a07ebcdf196dd6b17a
Faisal Ahmad <faisal152.fa@gmail.com>
2019-06-28 17:31:26 +0600

    Start

---
+++ app.js
@@ -0,0 +1,17 @@
+var express= require('express');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.send('Welcome to express server...');
+});
+
+app.listen(5000, function(){
+	console.log('Server started at 3000....');
+})
