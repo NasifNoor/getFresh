96e08b1a125df6280358d3df852067e01f298c25
Tanjil Ahmed <tanzil.ovi578@gmail.com>
2019-06-28 23:50:15 +0600

    404 error added

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
 	res.send('Welcome to express server...');
 });
+app.get('*',function(req,res){
+	res.send('404 error');
+})
 
 app.listen(5000, function(){
 	console.log('Server started at 3000....');
